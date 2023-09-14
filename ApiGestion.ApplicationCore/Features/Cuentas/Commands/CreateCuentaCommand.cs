9fede658345e1033b731b4b10efde76e45a16f6b
Dyuko <matias.irala.aveiro@gmail.com>
2023-09-14 15:03:20 -0400

    Crea estructura inicial de controllers, commands y queries

---
+++ ApiGestion.ApplicationCore/Features/Cuentas/Commands/CreateCuentaCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiGestion.ApplicationCore.Features.Cuentas.Commands
+{
+    internal class CreateCuentaCommand
+    {
+    }
+}
---
+++ ApiGestion.WebApi/Controllers/CuentasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGestion.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuentasController : ControllerBase
+    {
+    }
+}
